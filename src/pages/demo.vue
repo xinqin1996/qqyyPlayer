458041cab0afdd5f5ccbce36b2eee29c09d2a349
xinqin1996 <tnelswbh61088@163.com>
2019-12-23 10:40:38 +0800

    xinqin

---
+++ src/pages/demo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h2>这里是父组件</h2>
+    <my-demo></my-demo>
+  </div>
+</template>
+
+
+<script>
+import myDemo from "../components/demo/demo"
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components:{
+    myDemo
+  }
+}
+</script>
+
+<style>
+  .hello {
+    padding-top: 100px;
+  }
+</style>
